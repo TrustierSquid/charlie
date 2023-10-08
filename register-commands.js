935b57b356278dd15f9c2801fcae01cfc74a5d13
TrustierSquid <enthunt.sh@gmail.com>
2023-10-08 17:58:57 -0400

    added commands

---
+++ register-commands.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const {REST, Routes}= require('discord.js')
+
+const commands = [
+    {
+        name: 'forecast',
+        description: 'Returns your daily forecast',
+    },
+];
+
+const rest = new REST({version: '10'}).setToken(process.env.CLIENT_TOKEN);
+
+(async ()=> {
+    try {
+        console.log("Registering slash commands.....")
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.APPLICATION_ID, process.env.SERVER_ID),
+            { body: commands }
+        );
+
+        console.log("slash commands were successfully registered")
+    } catch (error) {
+        console.log(`There was an error ${error}`)
+    }
+})();
+
