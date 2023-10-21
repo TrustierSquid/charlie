94f235aa613df6e0d53380a8dc606d9c8d31c2ca
TrustierSquid <enthunt.sh@gmail.com>
2023-10-20 22:46:41 -0400

    Changed weather api to a more reliableapi. /forecast now displays current weather forecast for that day

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import { register, clientID } from "./register-commands.js";
 const geoKey = process.env['GEO_KEY'];
 const appID = process.env['APPLICATION_ID'];
+const weathKey = process.env['WEATHER_KEY']
 
 register(appID);
 
@@ -21,6 +22,14 @@ const charlie = new Client({
 
 charlie.once(Events.ClientReady, client => {
     console.log("Charlie has logged in and waiting....");
+
+    console.log("Current Commands:")
+    console.log("/forecast <postal code>")
+    
+    console.log("Shows the local forecast of the entered postal code \n")
+    
+    console.log("/servername")
+    console.log("Prints the server name in the console")
 })
 
 charlie.on('messageCreate', message => {
@@ -37,6 +46,9 @@ charlie.on('interactionCreate', interaction => {
     
     // forecast command displays weather forecast based on zipcode
     if (interaction.commandName === `forecast`) {
+        console.log("servername command ran in " + interaction.guild.name);
+
+
         // grabs the value of the argument (ZIPCODE)
         let userPostalCode = interaction.options.get('postalcode').value;
 
@@ -47,34 +59,57 @@ charlie.on('interactionCreate', interaction => {
         // Reverse geolocation API URL
         let geoURL = `https://api.geoapify.com/v1/geocode/search?text=${userPostalCode}&type=postcode&format=json&apiKey=${geoKey}`
         
-        // Weather API URL
+        
         // searching location and forecast
         let searchGeo = async ()=>{
             // All location data
+            // console.log(jsonResLocation.results[0]);
             const responseLocation = await fetch(geoURL)
             const jsonResLocation = await responseLocation.json();
-            latitude = jsonResLocation.results[0].latitude;
-            longitude = jsonResLocation.results[0].longitude;
-            console.log(jsonResLocation.results[0]);
-            console.log();
+            latitude = jsonResLocation.results[0].lat;
+            longitude = jsonResLocation.results[0].lon;
+            console.log("These are the geo API results for the postal code you entered...")
             
-            let weatherURL = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto`
+            // Key information about the searched postalcode
+            let city = jsonResLocation.results[0].city;
+            let state = jsonResLocation.results[0].state;            
             
-            // format for getting city info (jsonRes.results[0].city)
-
+            // format for getting city info (jsonRes.results[0].city);
+            
+            // Weather API URL
+            let weatherURL = `http://api.weatherapi.com/v1/forecast.json?key=${weathKey}&q=${city}&days=5`
+            
             let searchWeather = async ()=> {
                 const responseWeather = await fetch(weatherURL) 
                 const jsonResWeather = await responseWeather.json();
-                console.log(jsonResWeather) 
+                console.log("Here is the weather data based off of the postal code you entered")
+                // Weather api response    console.log("Current Commands:")
+
+
+                // The F at the end is for farenheit and C is for Celcius 
+                // All of the variables for the weather api
+                let output = (weatherResponse)=> {
+                    const currentTempF = Math.round(weatherResponse.current.temp_f) + '°';
+                    const currentTempC = Math.round(weatherResponse.current.temp_c) + '°';
+                    const condition = weatherResponse.current.condition.text;
+                    const windDirection = weatherResponse.current.wind_dir;
+                    const humidity = Math.round(weatherResponse.current.humidity);
+                    const feels_likeF = Math.round(weatherResponse.current.feelslike_f) + '°';
+                    const feels_likeC = Math.round(weatherResponse.current.feelslike_c) + '°';
+                    const wind_mph = Math.round(weatherResponse.current.wind_mph) + 'mph'; 
+                    const wind_kph = Math.round(weatherResponse.current.wind_kph) + 'kph';
+                    const weatherImg = weatherResponse.current.condition.icon;
+                    
+                    // All weather variables on display
+                    interaction.reply(`Today in ${city}, ${state}...\nIt is currently:\n\nCurrent Temperature - ${currentTempF}\nCondition for today - ${condition}\nWind Direction - ${windDirection}\nCurrent Humidity - ${humidity}\nFeels like - ${feels_likeF}\nWind Speed - ${wind_mph}`)
+                }
+
+                output(jsonResWeather)
 
             }
-
+            
             searchWeather();
 
-            let city = jsonResLocation.results[0].city
-
-            interaction.reply(`Today in ${city}...\n
-            It is currently: `)
         }
 
         searchGeo()
@@ -82,15 +117,7 @@ charlie.on('interactionCreate', interaction => {
     
     if (interaction.commandName === `servername`) {
         console.log("servername command ran in " + interaction.guild.name);
-        interaction.reply("You are currently in " + interaction.guild.name);
-    }
-    if (interaction.commandName === 'educate') {
-        console.log(interaction.user.displayName +  " ran the educate command.")
-        interaction.reply("You should not load commands in the `index.js` file with **__if statements__**. Create a method, and unforunately knowing how JS is, you may have to create a massive switch case inside that if statement. With Meloetta (shameless plug https://joshrandall.net/meloetta), we defined all the commands using functions, but that could just be how Python is vs JS.")
-    }
-    // Calling weather api based on what zip code the user entered.
-    async function callWeather(){
-        console.log('---------------------------------------')    
+        console.log(interaction)
     }
 
     
